1e05193ec46baf9b8fc0450e310cdc671a3e6ba3
Kai Lawrence <kai@dyn3193-126.wlan.ic.ac.uk>
2019-06-20 21:03:59 +0100

    better display of spike data

--- src/SNN_manyBoidsParallelVer/spikes_plot.py
+++ src/SNN_manyBoidsParallelVer/spikes_plot.py
@@ -33,17 +33,35 @@ for i in range(6):
         y_9.append(val[9])
         y_10.append(val[10])
 
-    plt.plot(time, y_0)
-    plt.plot(time, y_1)
-    plt.plot(time, y_2)
-    plt.plot(time, y_3)
-    plt.plot(time, y_4)
-    plt.plot(time, y_5)
-    plt.plot(time, y_6)
-    plt.plot(time, y_7)
-    plt.plot(time, y_8)
-    plt.plot(time, y_9)
-    plt.plot(time, y_10)
+    
+
+
+
+    fig, (ax0, ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8, ax9, ax10) = plt.subplots(11)
+
+    ax0.plot(0, 0, time, y_0)
+    ax1.plot(0, 1, time, y_1)
+    ax2.plot(0, 2, time, y_2)
+    ax3.plot(1, 0, time, y_3)
+    ax4.plot(1, 1, time, y_4)
+    ax5.plot(1, 2, time, y_5)
+    ax6.plot(2, 0, time, y_6)
+    ax7.plot(2, 1, time, y_7)
+    ax8.plot(2, 2, time, y_8)
+    ax9.plot(3, 0, time, y_9)
+    ax10.plot(3, 1, time, y_10)
+
+    ax0.set(ylabel='-150')
+    ax1.set(ylabel='-120')
+    ax2.set(ylabel='-90')
+    ax3.set(ylabel='-60')
+    ax4.set(ylabel='-30')
+    ax5.set(ylabel='0')
+    ax6.set(ylabel='30')
+    ax7.set(ylabel='60')
+    ax8.set(ylabel='90')
+    ax9.set(ylabel='120')
+    ax10.set(ylabel='150', xlabel='time(ms)')
     print('boid '+str(index))
     plt.show()
     index += 1
