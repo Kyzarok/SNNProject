56dd021291b6438fc0ec5c3ff2715cd47cdeb7c9
Kai Lawrence <kai@dyn3193-126.wlan.ic.ac.uk>
2019-06-20 21:13:36 +0100

    timing was off

--- src/SNN_manyBoidsParallelVer/spikes_plot.py
+++ src/SNN_manyBoidsParallelVer/spikes_plot.py
@@ -1,6 +1,5 @@
 import matplotlib.pyplot as plt
 import numpy as np
-from brian2 import *
 
 index = 0
 for i in range(6):
@@ -19,7 +18,7 @@ for i in range(6):
     y_9 = []
     y_10 = []
 
-    time = np.arange(len(big_array)) * (0.08*ms)
+    time = np.arange(len(big_array)) * (0.08)
     for val in big_array:
         y_0.append(val[0])
         y_1.append(val[1])
@@ -32,10 +31,6 @@ for i in range(6):
         y_8.append(val[8])
         y_9.append(val[9])
         y_10.append(val[10])
-
-    
-
-
 
     fig, (ax0, ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8, ax9, ax10) = plt.subplots(11)
 
@@ -61,7 +56,7 @@ for i in range(6):
     ax7.set(ylabel='60')
     ax8.set(ylabel='90')
     ax9.set(ylabel='120')
-    ax10.set(ylabel='150', xlabel='time(ms)')
+    ax10.set(ylabel='150', xlabel='time(s)')
     print('boid '+str(index))
     plt.show()
     index += 1
